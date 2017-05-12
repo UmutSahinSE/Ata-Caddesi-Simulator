c8562ae71c902e167d0d2cfcbe3ac3f4ce91b678
kerimcanuyanik <kerimcan--741s.t@hotmail.com>
2017-05-12 16:29:46 +0300

    Update

--- intersection.cpp
+++ intersection.cpp
@@ -4,6 +4,6 @@ intersection::intersection()
 {
     this->setGeometry(400,400,80,80);
     rectLine=new QRect(this->x()+100, this->y()+100, this->width(), this->height());
-    rectImage=new QPixmap(""); // Add intersection sign image!!! //
+    rectImage=new QPixmap(":/pictures/rectangle[737].png");
 
 }
